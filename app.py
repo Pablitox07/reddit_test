6511b37d28a355c7aa68cab747b7b261008979a9
Pablitox07 <118412580+Pablitox07@users.noreply.github.com>
2024-11-13 09:13:15 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/createPost')
+def createPost():
+    return render_template('createPost.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/posts/<post_id>')
+def view_posts(post_id):
+    return render_template('view_posts.html')
 if __name__ == '__main__':
     app.run(debug=True)
