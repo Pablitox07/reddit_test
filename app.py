bc22294404ea13562e5a2f451cd62637a0bb6a7c
Pablitox07 <118412580+Pablitox07@users.noreply.github.com>
2024-11-18 21:09:20 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, jsonify
+from datetime import datetime, timezone, timedelta
 import pyodbc
 import bcrypt
+import jwt
+import pytz
 
 connection_string = (
     "Driver={ODBC Driver 17 for SQL Server};"
@@ -9,7 +12,7 @@ connection_string = (
     "Trusted_Connection=Yes;"
 )
 
-
+SECRET_KEY = "vcwwue32jidnsv5ncdu1223349dbucwutang"
 
 
 try:
@@ -49,9 +52,10 @@ def profile(user_id):
 
 @app.route('/registerUser', methods=['POST'])
 def registerUser():
-    received_username = request.form.get('username')
-    received_email = request.form.get('email')
-    received_password = request.form.get('password_1')
+    received_username = request.json.get('username')
+    received_email = request.json.get('email')
+    received_password = request.json.get('password_1')
+
     try:
         conn = get_db_connection()
         cursor = conn.cursor()
@@ -79,9 +83,9 @@ def registerUser():
 
         # check that the email is unique
         if is_email_unique[0] > 0:
-            return "The email you entered is already in used."
+            return {"message" : "The email you entered is already in used.", "status": 400}
         elif is_username_unique[0] > 0:
-            return "The username you entered is already in used. "
+            return {"message" :"The username you entered is already in used. ", "status": 400}
         else:
             query_create_new_user = """
             INSERT INTO Users (email, password, username)
@@ -89,16 +93,34 @@ def registerUser():
             """
             cursor.execute(query_create_new_user,  (received_email, hashed_password, received_username))
             conn.commit()
-            return "The user was registered correctly."
+
+            # get the user current info
+            user_info = get_user_info(received_username)
+
+            # creates a new token with the user current info
+            token = create_token (user_info)
+            print(token)
+
+            return {
+                "message" :"The user was registered correctly.", 
+                "status": 200, 
+                "user_info": { 
+                "username": received_username, 
+                "profile_pic": user_info[3], 
+                "email":received_email, 
+                "user_id" : user_info[0]
+                },
+                "token": token
+            }
     except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e)}), 500
+        return {"message": str(e), "status": 500}
     finally:
         conn.close()
 
 @app.route('/loguser', methods=['POST'])
 def loguser():
-    received_user = request.form.get('username')
-    received_password = request.form.get('password')
+    received_user = request.json.get('username_email')
+    received_password = request.json.get('password')
     try:
         conn = get_db_connection()
         cursor = conn.cursor()
@@ -106,24 +128,92 @@ def loguser():
         # get username
         check_username = """
         SELECT * FROM Users
-        WHERE LOWER(username) = LOWER(?);
+        WHERE LOWER(username) = LOWER(?) OR LOWER(email) = LOWER(?);
         """ 
-        cursor.execute(check_username, (received_user))
-        username_unique = cursor.fetchone()
+        cursor.execute(check_username, (received_user, received_user))
+        user_info = cursor.fetchone()
 
-        if username_unique == None:
-            return "The user or the password were incorrect."
+        if user_info == None:
+            return {"message" :"The username and password are not correct.", "status": 400}
         else:
-            DB_password = username_unique[2]
+            DB_password = user_info[2]
             is_match = bcrypt.checkpw(received_password.encode('utf-8'), DB_password.encode('utf-8'))
             if is_match:
-                return "user logged"
+                # get the user current info            
+                user_info = get_user_info(received_user)
+
+                # creates a new token with the user current info
+                token = create_token (user_info)
+                print(token)
+
+                    
+                return {
+                    "message" :"The user was logged in correctly.", 
+                    "status": 200, 
+                    "user_info": { 
+                    "username": user_info[6], 
+                    "profile_pic": user_info[3], 
+                    "email":user_info[1], 
+                    "user_id" : user_info[0]
+                    },
+                    "token": token
+                }
             else:
-                return "The user or the password were incorrect."
+                return {"message" :"The username and password are not correct.", "status": 400}
     except Exception as e:
-        return jsonify({'status': 'error', 'message': str(e)}), 500
+        print(f"Error during login: {e}")
+        return {"message": "An unexpected error occurred. Please try again later.", "status": 500}, 500
+
     finally:
         conn.close()
+
+@app.route("/publishPost", methods=['POST'])
+def publishPost():
+    #get needed info from request 
+    received_userid = request.json.get('userid')
+    received_title = request.json.get("title")
+    received_posts_text = request.json.get("text")
+    received_category = request.json.get("category")
+    auth_header = request.headers.get('Authorization')
+
+    if not auth_header or not auth_header.startswith("Bearer "):
+        return {"message": "Missing or invalid token.", "status": 401}, 401
+    
+    token = auth_header.split(" ")[1]
+    
+    try:
+        # Decode and validate the token
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        query = """
+        INSERT INTO Posts (user_id, content, title, category)
+        VALUES 
+            (?, ?, ?, ?)
+        """
+        cursor.execute(query, (received_userid, received_posts_text, received_title, received_category))
+        conn.commit()
+
+        query_get_post_id = """
+        SELECT * FROM Posts
+        WHERE content = ? AND title = ? AND user_id = ?;
+        """
+        cursor.execute(query_get_post_id, (received_posts_text, received_title, received_userid))
+        new_post_row = cursor.fetchone()
+
+        return {"message": "Success", "post_id": new_post_row[0]}
+    
+
+
+    except jwt.ExpiredSignatureError as e:
+        print(e)
+        return {"message": "Token has expired.", "status": 401}, 401
+    except jwt.InvalidTokenError as e:
+        print(e)
+        return {"message": "Invalid token.", "status": 401}, 401
+    except Exception as e:
+        print(f"Error: {e}")
+        return {"message": "An unexpected error occurred. Please try again later.", "status": 500}, 500
 
 
 
@@ -138,7 +228,31 @@ def get_db_connection():
     )
     return pyodbc.connect(connection_string)
 
+def get_user_info(username):
+    conn = get_db_connection()
+    cursor = conn.cursor() 
+    query = """
+    SELECT * FROM Users
+    WHERE LOWER(username) = LOWER(?);
+    """
+    cursor.execute(query, (username,))
+    username_current_info = cursor.fetchone()
+    conn.commit()
+    conn.close()
+    return username_current_info
 
+def create_token (user_latest_info):
+    # below is an example of the info order
+    # 16, 'mf@ddom.com', '$2b$12$xafQ4jR6I8bwqGJZJbkMJeKt8pWtaaTRSCLFKv9mhclUbLbE46G.u', 'default_profile_img.png', 0, datetime.datetime(2024, 11, 16, 16, 43), 'mfdoom'
+    token = jwt.encode({
+    "user_id": user_latest_info[0],
+    "email": user_latest_info[1],
+    "password": user_latest_info[2],
+    "profile_likes": user_latest_info[4],
+    "exp": datetime.now(pytz.utc) + timedelta(hours=5), 
+    "username": user_latest_info[6],
+}, SECRET_KEY, algorithm="HS256")
+    return token
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/register.js
+++ static/register.js
@@ -1,18 +1,101 @@
+const website = "http://127.0.0.1:5000";
+
+
+function border_red_not(element) {
+    if (element.value === ""){
+        element.style.border = "1px solid red";
+    }
+    else{
+        element.style.border = "0px";
+    }
+}
+
+function something_wrong_inputs (email_string, username_string, password_1, password_2){
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const noSpaceRegex = /^\S+$/;
+    if (!emailRegex.test(email_string)){
+        return {"result": true, "message": "The format of the email is incorrect."};
+    }
+    else if (!noSpaceRegex.test(username_string)){
+        return {"result": true, "message": "username cannot contain spaces."};
+    }
+    else if (username_string.length < 3){
+        return {"result": true, "message": "username cannot be less than 3 characters."};
+    }
+    else if (username_string.length > 15){
+        return {"result": true, "message": "username cannot have more than 15 characters."};
+    }
+    else if (password_1 != password_2){
+        return {"result": true, "message": "The password must be the same."};
+    }
+    else if (password_1.length < 8){
+        return {"result": true, "message": "The password must be at least 8 characters long."};
+    }
+    else {
+        return {"result": false, "message": "correct"};
+    }
+}
+
 document.addEventListener("DOMContentLoaded", ()=> {
     const register_button = document.getElementById("register_button");
 
     register_button.addEventListener("click", ()=> {
+        // HTML inputs 
         const username = document.getElementById("username");
         const email = document.getElementById("email");
         const password_1 = document.getElementById("password_1");
         const password_2 = document.getElementById("password_2");
 
+        // gets the div where usewr will see info about the registration
+        let failed_message = document.getElementById("failed_message");
+
+        // shows which inputs must be filled by turning them red 
+        list_inputs = [username, email, password_1, password_2];
+        list_inputs.forEach(element => {
+            border_red_not(element);
+        });
+
+        const results_something_wrong_inputs = something_wrong_inputs(email.value, username.value, password_1.value, password_2.value);
+        
         if (username.value === "" || email.value === "" || password_1.value === "" || password_2.value === ""){
-            let failed_message = document.getElementById("failed_message");
             failed_message.innerHTML = '<h3 class="create_post_text">All fills must be filled.</h3><img src="../static/failed_face.jpg" alt="" srcset="" class="failed_face">';
         }
+        else if (results_something_wrong_inputs["result"]){
+            failed_message.innerHTML = `<h3 class="create_post_text">${results_something_wrong_inputs["message"]}</h3><img src="../static/failed_face.jpg" alt="" srcset="" class="failed_face">`;
+        }
+
+
+
+        // EXECUTE ONLY IF EVERYTHING ELSE IS CORECT 
         else {
-            
+            failed_message.innerHTML = "";
+            const params = { email: email.value, username: username.value, password_1: password_1.value };
+
+            fetch(`${website}/registerUser`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(params),
+            })
+                .then(response => {
+                    return response.json();
+                })
+                .then(data => {
+                    if (data["status"] >= 400){
+                        failed_message.innerHTML = `<h3 class="create_post_text">${data["message"]}</h3><img src="../static/failed_face.jpg" alt="" srcset="" class="failed_face">`;
+                    }
+                    else {
+                        list_cookies = ["valid_cookie", "username", "profile_pic", "email", "user_id"];
+                        list_cookies.forEach(cookie => {
+                            document.cookie = `${cookie}=${data[cookie]}; expires=${data["valid_cookie"]}; path=/`;
+                        });
+                        window.location.href = `${website}/`; 
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                });
         }
     });
 });
